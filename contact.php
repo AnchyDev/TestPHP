f4cab92eb33d109a035c49b223448dcabce5a798
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-11 11:05:44 +1000

    Forgot contact page.

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php 
+    $PAGE_TITLE = "Contact";
+    require_once('./format/head.php');
+?>
+
+<div class="header">
+    <a href="./home.php">Home</a>
+    <a href="./contact.php">Contact</a>
+</div>
+
+<div class="content">
+    Content
+</div>
+
+<div class="footer">
+    Footer
+</div>
+
+<?php require_once('./format/tail.php'); ?>
