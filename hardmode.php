c700d92115f8504b7a9c946ae16125ba2ba8560e
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-11 18:24:31 +1000

    Added submit button & test condition.

--- hardmode.php
+++ hardmode.php
@@ -3,12 +3,18 @@
     require_once('./format/head.php');
 
     $name = $_GET["name"];
+
+    if($name)
+    {
+        echo "<script type='text/javascript'>alert('$name');</script>";
+    }
 ?>
 
 <div class="content">
     <form action="./hardmode.php" method="GET">
         <label for="hm-name">Name</label>
         <input type="text" id="hm-name" name="name"><br><br>
+        <button type="submit">Create</button>
     </form>
     <div>
         <?php echo($name); ?>
