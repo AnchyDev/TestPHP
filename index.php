fa9864f298209f373fb747432d0bf727e5a3a4dc
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-12 18:44:02 +1000

    Cookie test.

--- index.php
+++ index.php
@@ -2,12 +2,35 @@
     $PAGE_TITLE = "Home";
     
     require_once('./format/head-start.php');
+?>
+<script>
+let cookieElement = document.getElementById("prompt-cookies");
+
+function EnableCookies()
+{
+    document.cookie = "allow-cookies=true; expires=Thu, 1 Dec 2024 12:00:00 UTC; path=/";
+    HideCookiePrompt();
+}
+
+function HideCookiePrompt()
+{
+    cookieElement.style.display = "none";
+}
+</script>
+<?php
     require_once('./format/head-end.php');
     require_once('./format/body-start.php');
 ?>
 
 <div class="content">
     Home Content
+
+    <?php
+    if(!isset($_COOKIE['allow-cookies'])) 
+    {
+        echo '<div id="prompt-cookies">Would you like to enable cookies? <button onclick="EnableCookies()">Yes</button><button onclick="HideCookiePrompt()">No</button></div>';
+    }
+    ?>
 </div>
 
 <?php require_once('./format/body-end.php'); ?>
