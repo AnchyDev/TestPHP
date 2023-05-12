49183b66e0e5ace3a7f0ac25405485a7723fe1b0
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-12 20:19:50 +1000

    Nicer button styles.

--- format/body-end.php
+++ format/body-end.php
@@ -1,7 +1,7 @@
             <?php
                 if(!isset($_COOKIE['allow-cookies'])) 
                 {
-                    echo '<div id="prompt-cookies" class="p-cookies">I use cookies to enhance your browser experience when you use my website. Would you like to enable cookies? <button onclick="EnableCookies()">Accept All</button><button onclick="HideCookiePrompt()">Reject All</button></div>';
+                    echo '<div id="prompt-cookies" class="p-cookies">I use cookies to enhance your browser experience when you use my website. Would you like to enable cookies? <button class="fs-nice-button" onclick="EnableCookies()">Accept All</button><button class="fs-nice-button" onclick="HideCookiePrompt()">Reject All</button></div>';
                 }
             ?>
             <div class="footer">
