e90057bfbbd2787708c94ed72a2f3644fcccab96
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-13 15:20:59 +1000

    Test 3.

--- common.php
+++ common.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
     define('REMOTE_ROOT_DIR', './TestPHP');
-    define('LOCAL_ROOT_DIR', '/var/www/html/TestPHP');
+    define('LOCAL_ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/TestPHP');
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
-echo($_SERVER['DOCUMENT_ROOT']);
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/common.php');
+
+    $PAGE_TITLE = "Home";
+
+    require_once(LOCAL_ROOT_DIR . '/format/head-start.php');
+    require_once(LOCAL_ROOT_DIR . '/format/head-end.php');
+    require_once(LOCAL_ROOT_DIR . '/format/body-start.php');
 ?>
+
+<div class="content">
+    Home Content
+</div>
+
+<?php require_once(LOCAL_ROOT_DIR . '/format/body-end.php'); ?>
