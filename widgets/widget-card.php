867ae67b4f2babb9f7cb0fb96b3945d9c44258a7
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-13 21:37:42 +1000

    Edit widget colors

--- widgets/widget-card.php
+++ widgets/widget-card.php
@@ -1,10 +1,10 @@
 <?php
     header("Content-Type: image/png");
 
-    $im = @imagecreate(110, 20) or die("Cannot Initialize new GD image stream");
+    $im = @imagecreate(256, 128) or die("Cannot Initialize new GD image stream");
 
     $background_color = imagecolorallocate($im, 0, 0, 0);
-    $text_color = imagecolorallocate($im, 233, 14, 91);
+    $text_color = imagecolorallocate($im, 0, 0, 255);
 
     imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
     imagepng($im);
