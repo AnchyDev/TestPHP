59d33add3e12251cf41e7d699864ee10665aef77
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-13 15:18:33 +1000

    Test 2.

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php 
 echo($_SERVER['DOCUMENT_ROOT']);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-    $PAGE_TITLE = "Home";
-
-    require_once("")
-    
-    require_once($LOCAL_ROOT_DIR . '/format/head-start.php');
-    require_once($LOCAL_ROOT_DIR . '/format/head-end.php');
-    require_once($LOCAL_ROOT_DIR . '/format/body-start.php');
 ?>
-
-<div class="content">
-    Home Content
-</div>
-
-<?php require_once($LOCAL_ROOT_DIR . '/format/body-end.php'); ?>
