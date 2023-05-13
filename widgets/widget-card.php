b67269b2eec83a56b8e611180acd4e21b80a2f7f
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-13 21:39:00 +1000

    Remove error supression.

--- widgets/widget-card.php
+++ widgets/widget-card.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-Type: image/png");
 
-    $im = @imagecreate(256, 128) or die("Cannot Initialize new GD image stream");
+    $im = imagecreate(256, 128) or die("Cannot Initialize new GD image stream");
 
     $background_color = imagecolorallocate($im, 0, 0, 0);
     $text_color = imagecolorallocate($im, 0, 0, 255);
