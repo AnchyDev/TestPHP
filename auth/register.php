86d406c6204dd5ac039b9b7ffabc41461aad28b9
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-13 10:53:56 +1000

    Cleanup form.

--- auth/register.php
+++ auth/register.php
@@ -10,17 +10,19 @@
     <form action="/auth/register.php" method="POST">
         <h1>Register</h1>
         <p>Please fill in this form to create an account.</p>
-        <hr>
 
-        <label for="username"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" id="username" required>
+        <br><hr><br>
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+        <label for="username"><b>Username</b></label><br>
+        <input type="text" placeholder="Enter Username" name="username" id="username" required><br><br>
 
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
-        <hr>
+        <label for="psw"><b>Password</b></label><br>
+        <input type="password" placeholder="Enter Password" name="psw" id="psw" required><br><br>
+
+        <label for="psw-repeat"><b>Repeat Password</b></label><br>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required><br><br>
+
+        <br><hr><br>
 
         <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
         <button type="submit" class="fs-nice-button">Register</button>
