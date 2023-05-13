4cf18fd30c6bba2fd085d665756472ae1cd8c332
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-13 10:27:29 +1000

    Test auth.

--- format/body-start.php
+++ format/body-start.php
@@ -6,6 +6,7 @@
             </a>
             <div class="nav-menu">
                 <a class="<?php echo ($PAGE_TITLE == "Home") ? "nav-item active" : "nav-item"; ?>" href="./index.php"><i class="ph ph-house"></i> Home</a>
+                <a class="<?php echo ($PAGE_TITLE == "Register") ? "nav-item active" : "nav-item"; ?>" href="./auth/register.php"><i class="ph ph-envelope-simple"></i> Register</a>
                 <a class="<?php echo ($PAGE_TITLE == "Contact") ? "nav-item active" : "nav-item"; ?>" href="./contact.php"><i class="ph ph-envelope-simple"></i> Contact</a>
             </div>
         </div>
