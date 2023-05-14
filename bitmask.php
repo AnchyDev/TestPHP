29a100f11c78c599c4c54560fec7f0c160a9abd8
AnchyDev <35346484+AnchyDev@users.noreply.github.com>
2023-05-14 10:36:29 +1000

    BigInt test 2.

--- bitmask.php
+++ bitmask.php
@@ -34,7 +34,7 @@
 
             for(let i = 0; i < itemCount; i++)
             {
-                let shiftValue = Number(BigInt.asUintN(itemCount, (1n << i)));
+                let shiftValue = Number(BigInt.asUintN(itemCount, BigInt(1 << i)));
                 let testElement = document.createElement("b");
                 testElement.innerHTML = `Element: ${shiftValue}`;
                 this.appendChild(testElement);
